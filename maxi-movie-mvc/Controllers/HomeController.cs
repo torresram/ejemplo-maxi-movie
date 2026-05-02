fca2583d9924ee45e81270755e98f3ff966da110
torresram <ramirotorres90@yahoo.com.ar>
2026-05-01 22:59:57 -0300

    cambios pequeños

--- maxi-movie-mvc/Controllers/HomeController.cs
+++ maxi-movie-mvc/Controllers/HomeController.cs
@@ -1,21 +1,48 @@
 using System.Diagnostics;
+using maxi_movie_mvc.Data;
 using maxi_movie_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace maxi_movie_mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MovieDbContext _context;
+        private const int PageSize = 8;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MovieDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int pagina = 1)
         {
-            return View();
+            if(pagina < 1)
+            {
+                pagina = 1;
+            }
+
+            var totalPeliculas = await _context.Peliculas.CountAsync();
+            var totalPaginas = (int)Math.Ceiling(totalPeliculas / (double)PageSize);
+
+            if(pagina > totalPaginas && totalPaginas > 0)
+            {
+                pagina = totalPaginas;
+            }
+
+            var peliculas = await _context.Peliculas
+                .Skip((pagina - 1) * PageSize)
+                .Take(PageSize)
+                .ToListAsync();
+
+            ViewBag.PaginaActual = pagina;
+            ViewBag.TotalPaginas = totalPaginas;
+            ViewBag.TotalPeliculas = totalPeliculas;
+
+            return View(peliculas);
         }
 
         public IActionResult Privacy()
